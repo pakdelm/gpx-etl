fc79e39a6db0c5d9dd8674f0f74eafd1f794d99b
pakdelm <mehrshad.pakdel@posteo.de>
2023-02-01 21:25:35 +0100

    change to ruff linter. update test

--- tests/process_test.py
+++ tests/process_test.py
@@ -1,18 +1,21 @@
+import logging
 import pytest
 
 import read
 from process import GPXDataFrameTransformer
 
+logger = logging.getLogger(__name__)
+
 TEST_FILES_DIR = "test_files"
+
 
 def test_label_distances():
 	gpx = read.read_gpx_file(f"{TEST_FILES_DIR}/gpx_distances.gpx")
 	gpx_transformer = GPXDataFrameTransformer(gpx)
-
 	df_gpx = gpx_transformer.converter.get_track_points()
-
 	df_distances = gpx_transformer.label_distances(df_gpx)
+	expected_distance = df_distances["distance"].sum()
 
-	sum_distance = df_distances["distance"].sum()
+	logger.debug(expected_distance)
 
-	assert (sum_distance) == pytest.approx(22600, 6)
+	assert expected_distance == pytest.approx(22600, abs=10)
