0f7013031a11849ec715c5bebad02eece164ebee
pakdelm <mehrshad.pakdel@posteo.de>
2023-04-29 22:44:06 +0200

    :white_check_mark: improve test utils

--- tests/convert_test.py
+++ tests/convert_test.py
@@ -1,15 +1,16 @@
 import logging
+from typing import List
 
 import pandas as pd
 import pytest
+from gpxpy.gpx import GPXTrackPoint
 
-from test_utils import add_sec
+import test_utils
 from utils import COLS
 
 from convert import GPXTransformer
 from read import read_gpx_file
 
-import gpxpy.gpx as mod_gpx
 from datetime import datetime, timedelta
 
 pd.set_option("display.max_columns", None)
@@ -19,41 +20,77 @@ logger = logging.getLogger(__name__)
 TEST_FILES_DIR = "test_files"
 
 
-@pytest.fixture
-def create_gpx_data() -> pd.DataFrame:
-	gpx = read_gpx_file(f"{TEST_FILES_DIR}/gpx_distances.gpx")
-	df_gpx = GPXTransformer(gpx).transform()
-	return df_gpx
+@pytest.fixture(scope="function")
+def create_gpx_data_from_class(request):
+    gpx = test_utils.generate_gpx_data(request.param)
+    df_gpx = GPXTransformer(gpx).transform()
+    return df_gpx
 
 
-def test_label_distances(create_gpx_data):
-	df_gpx = create_gpx_data
-	actual_distance = df_gpx["distance"].sum()
-
-	logger.debug(actual_distance)
-
-	assert actual_distance == pytest.approx(22600, abs=10)
+@pytest.fixture(scope="function")
+def create_gpx_data_from_file() -> pd.DataFrame:
+    gpx = read_gpx_file(f"{TEST_FILES_DIR}/gpx_distances.gpx")
+    df_gpx = GPXTransformer(gpx).transform()
+    return df_gpx
 
 
-def test_label_altitude():
-	start_time = datetime(2023, 1, 1, 0, 0, 0)
-	one_second = timedelta(seconds=1)
+@pytest.mark.parametrize(
+    "create_gpx_data_from_class",
+    [
+        [
+            GPXTrackPoint(latitude=40.73, longitude=-73.93),
+            GPXTrackPoint(latitude=34.05, longitude=-118.24)
+        ]
+    ],
+    indirect=True,
+)
+def test_label_long_distances(create_gpx_data_from_class):
+    df_gpx = create_gpx_data_from_class
+    distance = df_gpx[COLS.distance].sum()
+    assert distance == pytest.approx(3935.74 * 1000, rel=0.003) # 3 % error in long distances
 
-	gpx = mod_gpx.GPX()
-	gpx.tracks.append(mod_gpx.GPXTrack(name="test"))
-	gpx.tracks[0].segments.append(mod_gpx.GPXTrackSegment())
-	gpx.tracks[0].segments[0].points.append(
-		mod_gpx.GPXTrackPoint(latitude=12, longitude=13, elevation=90, time=start_time))
-	gpx.tracks[0].segments[0].points.append(
-		mod_gpx.GPXTrackPoint(latitude=12, longitude=13, elevation=100,
-							  time=add_sec(start_time, 1)))
-	gpx.tracks[0].segments[0].points.append(
-		mod_gpx.GPXTrackPoint(latitude=12, longitude=13, elevation=50,
-							  time=add_sec(start_time, 2)))
-	df_gpx = GPXTransformer(gpx).transform()
 
-	alt_gain = df_gpx[COLS.altitude_gain].sum()
-	alt_loss = df_gpx[COLS.altitude_loss].sum()
+@pytest.mark.parametrize(
+    "create_gpx_data_from_class",
+    [
+        [
+            GPXTrackPoint(latitude=40.75800984165629, longitude=-73.98555149950363),
+            GPXTrackPoint(latitude=40.75792384534224, longitude=-73.98557159337123),
+            GPXTrackPoint(latitude=40.75788219536777, longitude=-73.98560377987873),
+            GPXTrackPoint(latitude=40.7578319106077, longitude=-73.98565742405792),
+        ]
+    ],
+    indirect=True,
+)
+def test_label_short_distances(create_gpx_data_from_class):
+    df_gpx = create_gpx_data_from_class
+    distance = df_gpx[COLS.distance].sum()
+    assert distance == pytest.approx(22.3, rel=0.003) # 3 % error in long distances
 
-	assert alt_gain == 10
-	assert alt_loss == -50
+
+def test_label_distances_from_file(create_gpx_data_from_file):
+    df_gpx = create_gpx_data_from_file
+    actual_distance = df_gpx[COLS.distance].sum()
+
+    logger.debug(actual_distance)
+
+    assert actual_distance == pytest.approx(22600, abs=10)
+
+
+@pytest.mark.parametrize(
+    "create_gpx_data_from_class",
+    [
+        [
+            GPXTrackPoint(latitude=12, longitude=13, elevation=90),
+            GPXTrackPoint(latitude=12, longitude=13, elevation=100),
+            GPXTrackPoint(latitude=12, longitude=13, elevation=50),
+        ]
+    ],
+    indirect=True,
+)
+def test_label_altitude(create_gpx_data_from_class):
+    df_gpx = create_gpx_data_from_class
+    alt_gain = df_gpx[COLS.altitude_gain].sum()
+    alt_loss = df_gpx[COLS.altitude_loss].sum()
+    assert alt_gain == 10
+    assert alt_loss == -50
